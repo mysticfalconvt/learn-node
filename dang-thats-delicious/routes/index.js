2db2aad64ba17ca257636c3052139065ebf3e8c7
mysticfalconvt <rboskind@gmail.com>
2020-05-01 11:42:14 -0400

    tests

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-	res.send('Hey! It workz!');
+	res.send('Hey! It workzz!');
+	console.log('stuffsss');
 });
 
 module.exports = router;
+console.log('asdffffffff');
