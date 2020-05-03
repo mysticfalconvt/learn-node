29c87064f65b981fe5cf015dde4a4191fc3fd24d
mysticfalconvt <rboskind@gmail.com>
2020-05-03 09:52:29 -0400

    Stores are editable

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -20,3 +20,24 @@ exports.getStores = async (req,res) =>{
 	const stores = await Store.find();
 	res.render('stores', {title: 'Stores', stores: stores});
 };
+
+exports.editStore = async (req, res) => {
+	//find the store given id
+	const store = await Store.findOne({_id: req.params.id});
+
+	//confirm they are owner of the store
+
+	//render out the edit form so they can edit
+	res.render('editStore', {title: `edit ${store.name}`, store})
+};
+
+exports.updateStore = async (req, res) => {
+	// find and update store 
+	const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, { 
+		new: true, 
+		runValidators: true
+	}).exec();
+	// redirect to the store and tell them it worked
+	req.flash('success', `Sucessfully Updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+	res.redirect(`/stores/${store.id}/edit`);
+};
