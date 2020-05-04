fae5ab343fb9eddf80c6871b19004edfa0699281
mysticfalconvt <rboskind@gmail.com>
2020-05-03 20:25:59 -0400

    Added location lookup and update

--- starter-files/public/javascripts/modules/autocomplete.js
+++ starter-files/public/javascripts/modules/autocomplete.js
@@ -2,6 +2,17 @@ function autocomplete(input, latInput, lngInput) {
     if(!input) return;
     const dropdown = new google.maps.places.Autocomplete(input);
 
+    dropdown.addListener('place_changed', () => {
+        const place = dropdown.getPlace();
+        console.log(place);
+        lngInput.value = place.geometry.location.lng();
+        latInput.value = place.geometry.location.lat();
+    });
+    // if someone hits enter on the address form font submit
+    input.on('keydown', (e) => {
+        if(e.keyCode === 13) e.preventDefault();
+    });
 }
+
 
 export default autocomplete;
