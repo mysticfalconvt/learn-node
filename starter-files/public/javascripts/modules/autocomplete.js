8cf367bddf6e356ac4bcbd1ff61179348fac19e7
mysticfalconvt <rboskind@gmail.com>
2020-05-07 20:22:07 -0400

    cleaned up the dumps

--- starter-files/public/javascripts/modules/autocomplete.js
+++ starter-files/public/javascripts/modules/autocomplete.js
@@ -1,21 +1,16 @@
 function autocomplete(input, latInput, lngInput) {
-    if(!input) return;
-    const dropdown = new google.maps.places.Autocomplete(input);
+	if (!input) return;
+	const dropdown = new google.maps.places.Autocomplete(input);
 
-    dropdown.addListener('place_changed', () => {
-        const place = dropdown.getPlace();
-<<<<<<< HEAD
-=======
-        console.log(place);
->>>>>>> b6ca597af4c5313c3988fad972e352d6085e54e3
-        lngInput.value = place.geometry.location.lng();
-        latInput.value = place.geometry.location.lat();
-    });
-    // if someone hits enter on the address form font submit
-    input.on('keydown', (e) => {
-        if(e.keyCode === 13) e.preventDefault();
-    });
+	dropdown.addListener('place_changed', () => {
+		const place = dropdown.getPlace();
+		lngInput.value = place.geometry.location.lng();
+		latInput.value = place.geometry.location.lat();
+	});
+	// if someone hits enter on the address form font submit
+	input.on('keydown', (e) => {
+		if (e.keyCode === 13) e.preventDefault();
+	});
 }
-
 
 export default autocomplete;
