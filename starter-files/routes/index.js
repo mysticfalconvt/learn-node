c0480fc599e65247720c60f291cfb506e3fe560a
mysticfalconvt <rboskind@gmail.com>
2020-05-16 09:54:31 -0400

    Added check for logged in

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -2,27 +2,29 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore);
 
-router.post('/add', 
-    storeController.upload, 
-    catchErrors(storeController.resize), 
-    catchErrors(storeController.createStore)
+router.post(
+	'/add',
+	storeController.upload,
+	catchErrors(storeController.resize),
+	catchErrors(storeController.createStore),
 );
 
-router.post('/add/:id',
-    storeController.upload,
-    catchErrors(storeController.resize),
-    catchErrors(storeController.updateStore)
+router.post(
+	'/add/:id',
+	storeController.upload,
+	catchErrors(storeController.resize),
+	catchErrors(storeController.updateStore),
 );
 
-
-router.get('/stores/:id/edit',catchErrors(storeController.editStore));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
@@ -30,8 +32,10 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 
-router.post('/register', userController.validateRegister)
+router.post('/register', userController.validateRegister, userController.register, authController.login);
+router.get('/logout', authController.logout);
 
 module.exports = router;
